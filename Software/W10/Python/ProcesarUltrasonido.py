205a614febd96a7d891d9d98b397f830a0268e4f
Rodrig087 <miltonho@hotmail.com>
2021-11-08 16:17:37 -0500

    Se pusieron lo archivos en sus directorios correspondientes

--- Software/W10/Python/ProcesarUltrasonido.py
+++ Software/W10/Python/ProcesarUltrasonido.py
@@ -1,48 +1,110 @@
 #from matplotlib.widgets import Cursor
 import matplotlib.pyplot as plt
 import numpy as np
-from scipy.signal import hilbert
+from scipy.signal import hilbert,butter,filtfilt,find_peaks
+from scipy.interpolate import lagrange
+from scipy import signal
 #import time
 #import os
 #import errno
 
-#Variables:
+#Constantes:
 sizeTramaShort = 702
 sizeTramaInt = 350
+periodoMuestreo = 5
+
+#Variables:
+factorResample = 2
+periodoResample = periodoMuestreo/factorResample
+dix = 25*factorResample
 tramaDatosInt = []
 
 #Funciones:
-def CalcularValorAbsoluto(arrayMuestras):
+def RemuestrearOffset(arrayMuestras):
     #Convierte el array a formato np:
     npMuestras = np.array(arrayMuestras)
+    resampleMuestras = signal.resample(npMuestras, len(npMuestras) * factorResample)
     #Calcula la media:
-    meanMuestras = np.mean(npMuestras)
-    #meanMuestras = 508 #Medido con el osciloscopio
-    print(meanMuestras)
+    #meanMuestras = np.mean(resampleMuestras)
+    meanMuestras = 508 #Medido con el osciloscopio
+    #print(meanMuestras)
     #Realiza el offset de la señal:
-    npMuestras = npMuestras - meanMuestras
+    resampleMuestras = resampleMuestras - meanMuestras
     #Calcula el valora absoluto de la señal:
-    abMuestras = abs(npMuestras)
-    return abMuestras
-    #plt.plot(abMuestras)
-    #plt.show()
+    #abMuestras = abs(npMuestras)
+    return resampleMuestras
+        
+def butter_lowpass_filter(data, cutoff, fs, order):
+    nyq = 0.5 * fs  # Nyquist Frequency
+    normal_cutoff = cutoff / nyq
+    # Get the filter coefficients 
+    b, a = butter(order, normal_cutoff, btype='low', analog=False)
+    y = filtfilt(b, a, data)
+    return y
 
-def FiltrarSenal(senal):
-    print("Filtrando...")
+def DetectarEnvolvente(senal):
+    #print("Filtrando...")
     npSignal = np.array(senal)
     analytic_signal = hilbert(npSignal)
-    #amplitude_envelope = np.abs(analytic_signal) 
-    plt.plot(npSignal, label='signal')
-    plt.plot(analytic_signal, label='envelope')
-    plt.show()
+    amplitude_envelope = np.abs(analytic_signal) 
+    envolepe = butter_lowpass_filter(amplitude_envelope, 5547, 200000, 2)
+    #plt.plot(npSignal, label='signal')
+    #plt.plot(envolepe, label='envelope')
+    #plt.plot(peaks, envolepe[peaks], "x")
+    #plt.show()
+    return envolepe
 
+def InterpolarSenal(senal):
+    npSignal = np.array(senal)
+    y0 = np.max(npSignal)
+    x0 = list(npSignal).index(y0)
+    x1 = x0-dix
+    x2 = x0+dix
+    y1 = npSignal[x1]
+    y2 = npSignal[x2]
+    xp = [x1,x0,x2]
+    yp = [y1,y0,y2]
     
+    f = lagrange(xp,yp)
+    x_new = np.arange(0, 700,2.5)
+    
+    #plt.plot(x_new,f(x_new))
+    #plt.plot(xp, yp, "x")
+    #plt.show()
+    return f(x_new)
+    
+def CalcularPico(data):   
+    #funcion find_peaks de scipy:
+    #peaks, _ = find_peaks(data, prominence=1) 
+    #peaks, _ = find_peaks(data, height=0)
+    #print(peaks)
+    #Calcula el valor maximo y su indice:
+    npSignal = np.array(data)
+    y0 = np.max(npSignal)
+    x0 = list(npSignal).index(y0)
+    x1 = x0-dix
+    x2 = x0+dix
+    y1 = npSignal[x1]
+    y2 = npSignal[x2]
+    print(x0)
+    #print(y0)
+    nx = (y1-y2)/(2*(y1-(2*y0)+y2))                             
+    dx = nx*dix
+    tmax = x0
+    peak = tmax+dx
+    T2 = peak * periodoResample
+    print(peak)
+    print(T2)
+    #print(y0)
+    #print(y1)
+    #print(y2)
+        
 
 #Ingreso de datos:
 nombreArchivo = input("Ingrese el nombre del archivo: ")
 
 #Abre el archivo binario:
-path = str(nombreArchivo) + ".dat"
+path = "Datos/" + str(nombreArchivo) + ".dat"
 f = open(path, "rb")
 tramaDatosShort = np.fromfile(f, np.int8, sizeTramaShort)
 
@@ -81,12 +143,21 @@ for i in range(0, sizeTramaInt - 1):
 # datoPrueba = tramaDatosInt[184]
 # print("   DatoInt: " + hex(datoPrueba))
 
-plt.plot(tramaDatosInt)
-plt.show()
+#plt.plot(tramaDatosInt)
+#plt.show()
 
 #Procesamiento de la señal:
-banProcesar =  input("Desea extraer el evento? s/n: ")
+#banProcesar =  input("Desea extraer el evento? s/n: ")
+banProcesar = 's'
 if (banProcesar=='s'):
     print('Procesando...')
-    arraySenal = CalcularValorAbsoluto(tramaDatosInt)
-    FiltrarSenal(arraySenal)
+    offsetSenal = RemuestrearOffset(tramaDatosInt)
+    envolventeSenal = DetectarEnvolvente(offsetSenal)
+    CalcularPico(envolventeSenal)
+    interp = InterpolarSenal(envolventeSenal)
+    #Graficar:
+    plt.plot(offsetSenal)
+    plt.plot(envolventeSenal)
+    #plt.plot(interp)
+    #plt.plot(peaks, envolepe[peaks], "x")
+    plt.show()
