e9215948b294e6697befc9a8a06798d10e775dca
Rodrig087 <miltonho@hotmail.com>
2021-11-12 12:31:50 -0500

    Se modifico el programa AnalisisTemperatura-Distancia.py para realizar un analisis que permita establecer los valores de calibracion de TOF y temperatura en base a las mediciones de referencia. Luego se utilizaron estos datos para calcular la distancia en base a la desviacion de fase de la senal y se comprobo que el sensor tiene una repetibilidad excelente.

--- Software/W10/Python/AnalisisTemperatura-Distancia.py
+++ Software/W10/Python/AnalisisTemperatura-Distancia.py
@@ -1,15 +1,13 @@
 import matplotlib.pyplot as plt
 import numpy as np
 import math
-import time
-import os
-import errno
+
 
 Vtrama = []
 Dtrama = []
 
 TOF = 2.467 / 1000  #TOF en us
-Temp = np.linspace(0, 100, 10)  #Inicio, final, numero de puntos
+Temp = np.linspace(0, 20, 10)  #Inicio, final, numero de puntos
 
 #Calcula la velocidad del sonido:
 for i in range(0, (len(Temp))):
@@ -24,18 +22,40 @@ for i in range(0, (len(Temp))):
 VariacionVel = Dtrama[len(Temp) - 1] - Dtrama[0]
 print(str(VariacionVel))
 
+#******************************************************************************
+#Calibracion del sensor:
+Altura = 250
+Temperatura = 12.62
+TOF = 1562.5
+Vsonido = 331.45 * math.sqrt(1 + (Temperatura / 273))
+
+Distancia = 0.5*(TOF/1e6)*Vsonido*1000
+TOF_rev = (1e6)*(Altura*2)/(Vsonido*1000)
+dTOF = 1562.5-TOF_rev
+
+print("Altura [mm]: %f" % Altura) 
+print("Temperatura [gc]: %f" % Temperatura) 
+print("TOF [us]: %f" % TOF) 
+print("Vsonido [mm]: %f" % Vsonido) 
+print("")
+print("Distancia [mm]: %f" % Distancia)
+print("TOF revisado [us]: %f" % TOF_rev)
+print("dTOF revisado [us]: %f" % dTOF)
+
+#******************************************************************************
+
 #Grafica los resultados:
-fig = plt.figure()
-ax1 = fig.add_subplot(211)
-plt.plot(Temp, Vtrama)
-plt.setp(ax1.get_xticklabels(), visible=False)
-plt.title('Velocidad del sonido')
-plt.ylabel('m/seg')
+# fig = plt.figure()
+# ax1 = fig.add_subplot(211)
+# plt.plot(Temp, Vtrama)
+# plt.setp(ax1.get_xticklabels(), visible=False)
+# plt.title('Velocidad del sonido')
+# plt.ylabel('m/seg')
 
-ax2 = plt.subplot(212, sharex=ax1)
-plt.plot(Temp, Dtrama)
-plt.title('Distancia')
-plt.ylabel('mm')
-plt.xlabel('Temperatura [oC]')
+# ax2 = plt.subplot(212, sharex=ax1)
+# plt.plot(Temp, Dtrama)
+# plt.title('Distancia')
+# plt.ylabel('mm')
+# plt.xlabel('Temperatura [oC]')
 
-plt.show()
+# plt.show()
--- Software/W10/Python/PruebaCorrelacion.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Tue Nov  9 09:37:32 2021
-
-@author: ivan.palacios
-"""
-
-
